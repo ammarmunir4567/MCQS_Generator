ec82c7375b07ca52de459fdd85dde7faf2b3fec2
Ammar Munir <105548542+ammarmunir4567@users.noreply.github.com>
2025-02-24 17:47:21 +0500

    Update llm.py

--- llm.py
+++ llm.py
@@ -6,6 +6,6 @@ import os
 
 load_dotenv()
 def init_llm(temp,token_size):
-    llm = ChatGroq(temperature=temp,verbose=True,max_tokens=token_size, groq_api_key=os.getenv('GROQ_API'), model_name="llama-3.1-70b-versatile")
+    llm = ChatGroq(temperature=temp,verbose=True,max_tokens=token_size, groq_api_key=os.getenv('GROQ_API'), model_name="llama-3.3-70b-versatile")
     print("token size is ",token_size)
     return llm
